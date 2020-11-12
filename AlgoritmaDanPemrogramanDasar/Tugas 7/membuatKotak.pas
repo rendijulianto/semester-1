13620c77aaedefe3c2db715d7634ba9b5aa3c7dc
rendijulianto <rendijulianto1707@gmail.com>
2020-11-12 22:30:22 +0800

    edit

--- AlgoritmaDanPemrogramanDasar/Tugas 7/membuatKotak.pas
+++ AlgoritmaDanPemrogramanDasar/Tugas 7/membuatKotak.pas
@@ -2,44 +2,38 @@ program ulang_kotak;
 uses crt;
 var i,x1,x2,y1,y2 : integer;
 begin
-   write('Posisi X1 : ');readln(x1);
-   write('Posisi Y1 : ');readln(y1);
-   write('Posisi X2 : ');readln(x2);
-   write('Posisi Y2 : ');readln(y2);
+     write('X1 : ');readln(x1);
+	 write('Y1 : ');readln(y1);
+	 write('X2 : ');readln(x2);
+	 write('Y2 : ');readln(y2);
 
-
-   ClrScr;
-   //X1
-  gotoxy(x1,y1);
-  write('+');
-  for i:=1 to x2-x1 do
-  begin
-  gotoxy(x1+i,y1);
-  write('-');
-  end;
-  write('+');
-
-  for i:=1 to y2-y1 do
-  begin
-  gotoxy(x2+1,y1+i);
-  write('|');
-  end;
-
-
-  for i:=1 to x2-x1 do
-  begin
-  gotoxy(x1+i,y2);
-  write('-');
-  end;
-
-  write('+');
-  for i:=1 to y2-y1 do
-  begin
-  gotoxy(x1,y1+i);
-  write('|');
-  end;
-  gotoxy(x1,y2);
-  write('+');
+     gotoxy(x1,y1);
+     write('+');
+     for i:=1 to x2-x1-1 do
+     begin
+      gotoxy(x1+i,y1);
+      write('-');
+     end;
+     write('+');
+     for i:=1 to y2-x1+1 do
+     begin
+      gotoxy(x2,y1+i);
+      write('|');
+     end;
+     gotoxy(x2,y2);
+     write('+');
+     for i:=1 to x2-x1-1 do
+     begin
+     gotoxy(x1+i,y2);
+     write('-');
+     end;
+     gotoxy(x1,y2);
+     write('+');
+     for i:=1 to y2-x1+1 do
+     begin
+     gotoxy(x1,y1+i);
+     write('|');
+     end;
 
 
 
